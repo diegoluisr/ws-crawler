a2a4fd2e8ac4b8307b2532b0dcf497be9bbddc91
Diego Restrepo <diegoluisr@gmail.com>
2024-07-03 17:20:08 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Enabling VENV (Virtual Environment) for this project.
+`python3 -m venv .venv`
+
+Updating PIP to its latest version.
+`python3 -m pip install --upgrade pip`
+
+Launching working environment.
+`source ./.venv/bin/activate`
+
+Installing dependencies/requirements.
+`pip install -r requirements.txt`
+
+Use next command to consolidate all dependencies in a single file.
+`pip freeze > requirements.txt`
+
+Running the application.
+`python3 main.py`
---
+++ main.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from robots import RobotsParser
+import datetime
+import hashlib
+import os
+import requests
+import time
+import yaml
+
+class SpecimenAnalyser:
+
+  id = None
+  robotParser = None
+  driver = None
+  folder_path = None
+  items = []
+
+  def __init__(self, id, baseUrl, robots):
+    self.id = id
+    self.folder_path = f'data/{self.id}'
+    os.makedirs(self.folder_path, exist_ok = True)
+    robotFile = requests.get(baseUrl + robots)
+    if robotFile.status_code == 200:
+      self.robotParser = RobotsParser.from_string(robotFile.text)
+
+  def checkUrl(self, userAgent, path):
+    if self.robotParser != None:
+      return self.robotParser.can_fetch(userAgent, path)
+    return False
+
+  def loadPage(self, url):
+    if self.driver == None:
+      options = Options()
+      options.add_argument('--headless')  # Ejecución sin interfaz gráfica
+      driver = webdriver.Firefox(options = options)
+    driver.get(url.replace('https://', 'https://bbdev:bbdev@'))
+    time.sleep(10)
+    return driver.page_source
+
+  def addLink(self, url):
+    # if not site.checkUrl('spider:aa', '/'):
+    #   return []
+    if url == '/':
+      url = specimen['base_url']
+    elif url.startswith('/'):
+      url = specimen['base_url'] + url
+    elif url.startswith(specimen['base_url']):
+      pass
+    else:
+      return []
+    file_path = self.urlToFile(url)
+    print(url, file_path)
+    # return ['/Blackboard_Data']
+    if not os.path.isfile(file_path):
+      with open(file_path, 'w') as file:
+        content = self.loadPage(url)
+        file.write(content)
+        file.close()
+        soup = BeautifulSoup(content, 'html.parser')
+        links = soup.find_all('a', href=True)
+        return set(link['href'] for link in links)
+    return []
+
+  def urlToFile(self, url):
+    url = url.replace(specimen['base_url'], '')
+    parts = url.split('/')
+    if len(parts) == 0 or (len(parts) == 1 and parts[0] == ''):
+      return self.folder_path + '/index.html'
+    else:
+      os.makedirs(self.folder_path + '/'.join(parts), exist_ok = True)
+      return self.folder_path + '/'.join(parts)  + '/index.html'
+
+  def close(self):
+    self.driver.close()
+
+with open('specimens.yml', 'r') as file:
+  config = yaml.safe_load(file)
+
+for specimen in config['specimens']:
+  site = SpecimenAnalyser(specimen['id'], specimen['base_url'], '/robots.txt')
+  pages = site.addLink('/')
+  while len(pages) > 0:
+    url = pages.pop()
+    links = site.addLink(url)
+    pages = list(pages) + list(links)
+    pages = list(dict.fromkeys(pages))
+    # break
+  site.close()
+  # break
